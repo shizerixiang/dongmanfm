b1aaf84b35302a1a2df15c718a92429ccffb214a
shize <shizerixiang@hotmail.com>
2017-11-27 15:53:32 +0800

    给漫单创建接口添加编辑功能，给搜索接口添加清空历史搜索记录的功能

--- contractlib/src/main/java/fm/dongman/animefm/contractlib/contract/SearchContract.kt
+++ contractlib/src/main/java/fm/dongman/animefm/contractlib/contract/SearchContract.kt
@@ -26,15 +26,35 @@ interface SearchContract {
          * @param hotList 热门搜索
          */
         fun showHotSearch(hotList: List<String>)
+
+        /**
+         * 显示清除历史搜索记录结果
+         * 由 [ISearchPresenter.clearHistorySearch] 回调
+         * @param boolean 是否清除成功
+         */
+        fun showClearHistoryState(boolean: Boolean)
     }
 
     /**
      * 搜索操作接口
      */
-    interface ISearchPresenter : BasePresenter
+    interface ISearchPresenter : BasePresenter{
+        /**
+         * 清除历史搜索记录
+         * 异步调用 [ISearchDataSource.clearHistorySearch] 清除数据
+         * 并在主线程中调用 [ISearchView.showClearHistoryState] 显示清除结果
+         */
+        fun clearHistorySearch()
+    }
 
     /**
      * 搜索数据源接口
      */
-    interface ISearchDataSource : BaseDataSource<SearchDataModel>
+    interface ISearchDataSource : BaseDataSource<SearchDataModel>{
+        /**
+         * 清除历史搜索记录
+         * @param callback 清除回调
+         */
+        fun clearHistorySearch(callback: BaseDataSource.LoadSourceCallback<String>)
+    }
 }
--- contractlib/src/main/java/fm/dongman/animefm/contractlib/contract/SheetCreateContract.kt
+++ contractlib/src/main/java/fm/dongman/animefm/contractlib/contract/SheetCreateContract.kt
@@ -25,6 +25,20 @@ interface SheetCreateContract {
          * 由 [ISheetCreatePresenter.createSheet] 回调
          */
         fun showCreateFailed()
+
+        /**
+         * 显示编辑漫单的信息
+         * 由 [ISheetCreatePresenter.setEditSheetId] 回调
+         * @param sheet 需要编辑的漫单数据
+         */
+        fun showEditSheetInfo(sheet:IModel.ISheetModel)
+
+        /**
+         * 显示编辑漫单状态
+         * 由 [ISheetCreatePresenter.editSheet] 回调
+         * @param boolean 是否成功修改漫单数据
+         */
+        fun showEditState(boolean: Boolean)
     }
 
     /**
@@ -32,24 +46,53 @@ interface SheetCreateContract {
      */
     interface ISheetCreatePresenter:BasePresenter{
         /**
+         * 设置编辑漫单的 id
+         * 通过 设置漫单id [ISheetCreateDataSource.setEditSheetId] 再异步调用 [ISheetCreateDataSource.getData] 获取要编辑的漫单数据
+         * 并在 UI 线程中调用 [ISheetCreateView.showEditSheetInfo] 显示漫单数据
+         * @param sheetId 编辑漫单的 id
+         */
+        fun setEditSheetId(sheetId:String)
+
+        /**
          * 创建漫单
          * 通过异步调用 [ISheetCreateDataSource.createSheet] 获取数据
          * 并在 UI 线程中调用 [ISheetCreateView.showCreateSucceed] 或 [ISheetCreateView.showCreateFailed] 显示数据
          * @param sheet 漫单
          */
         fun createSheet(sheet: IModel.ISheetModel)
+
+        /**
+         * 编辑漫单
+         * 通过异步调用 [ISheetCreateDataSource.editSheet] 获取数据
+         * 并在 UI 线程中调用 [ISheetCreateView.showEditState] 显示数据
+         * @param sheet 修改后的漫单
+         */
+        fun editSheet(sheet: IModel.ISheetModel)
     }
 
     /**
      * 漫单数据源接口
      */
-    interface ISheetCreateDataSource:BaseDataSource<Void>{
+    interface ISheetCreateDataSource:BaseDataSource<IModel.IUserModel>{
+        /**
+         * 设置编辑漫单的 id
+         * @param sheetId 漫单 id
+         */
+        fun setEditSheetId(sheetId: String)
+
         /**
          * 创建漫单
          * @param userId 用户 id
          * @param sheet 漫单
          * @param callback 获取信息回调接口
          */
-        fun createSheet(userId: String, sheet: IModel.ISheetModel, callback: BaseDataSource.LoadSourceCallback<Void>)
+        fun createSheet(userId: String, sheet: IModel.ISheetModel, callback: BaseDataSource.LoadSourceCallback<String?>)
+
+        /**
+         * 提交编辑漫单信息
+         * @param sheet 编辑后的漫单
+         * @param callback 获取信息回调接口
+         */
+        fun editSheet(sheet:IModel.ISheetModel, callback: BaseDataSource.LoadSourceCallback<String?>)
     }
 }
